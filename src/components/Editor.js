2e5717b82ce36f5e83320167e87fe9210839e2e4
Gurveen <gurveens11@gmail.com>
2021-01-20 00:28:08 +0530

    Style open-close icons

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
 import "codemirror/mode/css/css";
 import "codemirror/mode/javascript/javascript";
 import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Editor = (props) => {
   const { displayName, language, value, onChange } = props;
+
+  const [open, setOpen] = useState(true);
 
   const handleChange = (editor, data, value) => {
     onChange(value);
   };
 
   return (
-    <div className="editor-container">
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
       <div className="editor-title">
-        {displayName} <button>O/C</button>
+        {displayName}{" "}
+        <button
+          type="button"
+          className="expand-collapse-btn"
+          onClick={() => {
+            setOpen((prevOpen) => !prevOpen);
+          }}
+        >
+          <FontAwesomeIcon
+            icon={open ? faCompressAlt : faExpandAlt}
+          ></FontAwesomeIcon>
+        </button>
       </div>
       <ControlledEditor
         className="code-mirror-wrapper"
